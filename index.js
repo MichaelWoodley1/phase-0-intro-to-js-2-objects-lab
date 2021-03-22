4a9c2a384ead3a8954106880e4bf73836099e1e7
Michael <mawwoodley@gmail.com>
2021-03-21 21:00:48 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+let employee = {
+    name: 
+        'Mike',
+    streetAddress: 
+        'Easy Street'
+    
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   const newEmployee = { ...employee};
+   //will create a clone of original object and save it into a new variable
+   newEmployee[key] = value;
+   return newEmployee;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const employee = Object.assign({}, obj);
+    delete employee[key];
+    return employee
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
